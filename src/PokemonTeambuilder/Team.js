c976182df9bc75f3cc861591ac2d69ae0b96e84a
James Wu <wu.james@hotmail.com>
2021-11-24 11:21:56 -0500

    team feature

---
+++ src/PokemonTeambuilder/Team.js
@@ -0,0 +1,26 @@
+
+import { Component } from "react";
+
+export default class Team extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    
+    render() {
+        const { name,sprite } = this.props
+        if (name && sprite) {
+            return (
+                <div className="teamPokemon">
+                    <img src={sprite} alt="" />
+                    <p>{name}</p>
+                </div>
+            )
+        } else {
+            return(
+                <div></div>
+            )
+        }
+
+    }
+}
