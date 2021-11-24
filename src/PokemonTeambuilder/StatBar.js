e6026d4e6222321e4290d123130d2b03b05c1a2d
James Wu <wu.james@hotmail.com>
2021-11-24 18:29:46 -0500

    types

--- src/PokemonTeambuilder/StatBar.js
+++ src/PokemonTeambuilder/StatBar.js
@@ -9,6 +9,8 @@ export default class StatBar extends Component {
     render() {
         const {stat} = this.props;
         if (stat) {
+            // i could not think of another way to link the size of the
+            // div to the value of the element without using inline jsx
             return (
                 <div className="statBar" style={{width:`${stat}px`}}></div>
             )
